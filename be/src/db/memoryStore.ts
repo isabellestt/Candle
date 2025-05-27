ddfe89b3c5f7d907e785dc677e506a79270fe5c1
isabellestt <seetisabelle@gmail.com>
2025-05-27 18:09:58 +0800

    Clean up

--- be/src/db/memoryStore.ts
+++ be/src/db/memoryStore.ts
@@ -1,8 +1,8 @@
-import {FunctionCallMessageResponse, EndOfCallReportMessageResponse} from "../types";
+import {ToolCallMessageResponse, EndOfCallReportMessageResponse} from "../types";
 
 export interface UnifiedCallLog {
   callId: string;
-  functionCall?: FunctionCallMessageResponse;
+  functionCall?: ToolCallMessageResponse;
   endOfCallReport?: EndOfCallReportMessageResponse;
 }
 export const memoryStore: Record<string, UnifiedCallLog> = {};
