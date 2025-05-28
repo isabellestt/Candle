95c89af849bf012abbef34eab26978240294145b
isabellestt <seetisabelle@gmail.com>
2025-05-28 14:48:15 +0800

    Update demo route

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import Dashboard  from './routes/Dashboard.tsx'
+import Dashboard  from './routes/Demo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
