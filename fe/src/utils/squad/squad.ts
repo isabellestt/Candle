e48d8b0926db14f4921ce28919c3ff5b8636981e
isabellestt <seetisabelle@gmail.com>
2025-06-15 12:12:57 +0800

    Update squad message and helpline assistant system prompt

--- fe/src/utils/squad/squad.ts
+++ fe/src/utils/squad/squad.ts
@@ -1,6 +1,8 @@
 import type { CreateSquadDTO } from "@vapi-ai/web/dist/api";
 import { helplineAssistant } from "../assistant/helpline.assistant";
-import { authorityAssistant } from "../assistant/authorities.assistant";
+// import { authorityAssistant } from "../assistant/authorities.assistant";
+
+const assistant = import.meta.env.VITE_VAPI_AUTHORITY_ASSISTANT_ID
 
 export const squad : CreateSquadDTO ={
   name: "helpline to authorities",
@@ -9,8 +11,7 @@ export const squad : CreateSquadDTO ={
       assistant: helplineAssistant,
       assistantDestinations: [
         {
-          message: "abracadabra, I am now connecting you to the sushi authorities. Please hold on for a moment.",
-          // message: "I'll connect you to the police via 999; we'll stay on the line together. Okay great, I am on the phone with an officer from {{agency}} and I've relayed the details of your situation with them. Okay they are ready to speak with you, I’ll transfer you now, take care and goodbye.",
+          message: "I'll connect you to the police via 999; we'll stay on the line together. Okay great, I am on the phone with an officer from {{agency}} and I've relayed the details of your situation with them. Okay they are ready to speak with you, I’ll transfer you now, take care and goodbye.",
           description: "Transfer call to Authorities assistant",
           type: "assistant",
           assistantName: "Authorities",
@@ -18,7 +19,7 @@ export const squad : CreateSquadDTO ={
       ]
     },
     {
-      assistant: authorityAssistant,
+      assistantId: assistant,
       assistantDestinations: []
     },
   ] 
