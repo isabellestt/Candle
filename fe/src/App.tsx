7e4c782b700060731eda7710d3775b1bbbd6cfa7
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2025-08-22 20:53:30 +0800

    Unarchive candling demo page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,26 +1,11 @@
 // import { NavLink } from 'react-router'
-// import { Header } from "./components/landing/header/Header";
-// import { Hero } from "./components/landing/hero/Hero";
-// import { Marquee } from "./components/landing/marquee/Marquee";
-// import { AudienceSegments } from "./components/landing/segment/AudienceSegments";
-// import { CandlingInsights } from "./components/landing/insights/CandlingInsights";
-// import { Vapi } from "./components/landing/vapi/Vapi";
-// import { Footer } from "./components/landing/footer/Footer";
+
 import { CandleHeader } from "./components/candle-landing/Candle-Header";
 
 function App() {
   return (
     <>
       <CandleHeader />
-      {/* <div className="main-app">
-        <Header />
-        <Hero />
-        <Marquee />
-        <AudienceSegments />
-        <CandlingInsights />
-        <Vapi />
-        <Footer />
-      </div> */}
     </>
     // <div className="flex flex-col items-center justify-center">
     //   cooking
--- fe/src/utils/assistant/useVapi.ts
+++ fe/src/utils/assistant/useVapi.ts
@@ -2,7 +2,7 @@ import type { AssistantOverrides } from "./../../../node_modules/@vapi-ai/web/di
 import { useEffect, useState, useRef } from "react";
 import { vapi } from "./vapi.sdk";
 import type { CallRecord } from "./../../types/conversation.type";
-import { helplineAssistant } from "./helpline.assistant";
+// import { helplineAssistant } from "./helpline.assistant";
 import { squad } from "../squad/squad";
 import { callData as defaultCallData } from "../../../public/callData";
 import formatDateForDisplay from "../formatDate";
@@ -164,6 +164,7 @@ export function useVapi() {
           agency: "",
         },
       };
+      const helplineAssistant = import.meta.env.VITE_PUBLIC_VAPI_AUTHORITY_ASSISTANT_ID
       const res = await vapi.start(
         helplineAssistant,
         assistantOverrides,
